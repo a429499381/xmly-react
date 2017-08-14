d9cc2594888ed22b293f4771d9a5c11def59b4ab
xutao <429499381@qq.com>
2017-08-14 20:35:05 +0800

    导入以前做的喜马拉雅静态文件

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <div className="top">
+        <a href="#" className="top_l">
+          城市
+          <i className="top_l_icon"></i>
+        </a>
+        <div className="search">
+          <i className="search_icon"></i>
+          <input type="text" placeholder="请输入要搜索的关键字"/>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <a href="#" className="top_r">
+          用户
+        </a>
+      </div>
       </div>
     );
   }
