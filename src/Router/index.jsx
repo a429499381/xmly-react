f270d107a005c8d827bc1049c965029638505a62
xutao <429499381@qq.com>
2017-08-14 23:01:17 +0800

    引入 路由 出错

---
+++ src/Router/index.jsx
@@ -0,0 +1,32 @@
+/**
+ * Created by xutao on 2017/8/14.
+ */
+import React from 'react'
+import { Router, Route, IndexRoute } from 'react-router'
+
+import App from '../App'
+import Home from '../Home/index'
+
+
+// 如果是大型项目，router部分就需要做更加复杂的配置
+// 参见 https://github.com/reactjs/react-router/tree/master/examples/huge-apps
+
+class RouterMap extends React.Component {
+  render() {
+    return (
+      <Router history={this.props.history}>
+        <Route path='/' component={App}>
+          <IndexRoute component={Home}/>
+          {/*<Route path='/city' component={City}/>*/}
+          {/*<Route path='/Login(/:router)' component={Login}/>*/}
+          {/*<Route path='/User' component={User}/>*/}
+          {/*<Route path='/search/:category(/:keyword)' component={Search}/>*/}
+          {/*<Route path='/detail/:id' component={Detail}/>*/}
+          {/*<Route path='*' component={NotFound}/>*/}
+        </Route>
+      </Router>
+    )
+  }
+}
+
+export default RouterMap
--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { hashHistory } from 'react-router'
+
+import RouteMap from './Router/index'
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <RouteMap history={hashHistory}/>,<App />, document.getElementById('root'));
 registerServiceWorker();
+
+
+
+// import RouteMap from './router/routeMap'
+//
+// render(
+//   <Provider store={store}>
+//     <RouteMap history={hashHistory}/>
+//   </Provider>,
+//   document.getElementById('root')
+// )
