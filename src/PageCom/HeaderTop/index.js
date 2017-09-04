1b51825cec21125108737acd1e7d12b5adf6575f
xutao <429499381@qq.com>
2017-09-04 15:26:25 +0800

    搜索 input 处理 ok

--- src/PageCom/HeaderTop/index.js
+++ src/PageCom/HeaderTop/index.js
@@ -4,8 +4,15 @@ import {Link} from 'react-router'
 import './index.css'
 
 class HeaderTop extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            data: '',
+            value: ''
+        }
+    }
   render() {
-      let data = this.props.data
+      let data = this.state.data
     return (
       <div className="top">
         {/* top */}
@@ -16,7 +23,11 @@ class HeaderTop extends Component {
               </span>&nbsp;
             </Link>
             <div className="input dib">
-              <input type="text" placeholder={data.value}/>
+              <input type="text" placeholder={data.value}
+                      value ={this.state.value}
+                      onChange={this.ChangeHandle.bind(this)}
+                      onKeyUp={this.KeyCode.bind(this)}
+              />
             </div>&nbsp;
             <span className="top_r dib">
             <a href="/Ls" className="lishi">{data.lishi}</a>
@@ -27,6 +38,25 @@ class HeaderTop extends Component {
         {/* top */}
     </div>
     )
+  }
+  componentDidMount() {
+      // 映射 父 组件传来数据  this.props.data 到 this.state.data
+      this.setState({
+          data: this.props.data
+      })
+  }
+  // 每按一个键就吧 获取的字符 映射到 this.state.value
+  ChangeHandle(e) {
+        this.setState({
+            value: e.target.value
+        })
+  }
+  // 回车 处理  13 代码回车键
+  KeyCode(e) {
+    if(e.keyCode !== 13) {
+        return
+    }
+
   }
 
 }
