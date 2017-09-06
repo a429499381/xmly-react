920b4c7dfa49a622e20a3086ee0a847851f3edc3
xutao <429499381@qq.com>
2017-09-06 17:18:33 +0800

    循环写入 整理来的数据。 失败

--- src/DB/HomeDB.js
+++ src/DB/HomeDB.js
@@ -7,32 +7,26 @@ import {DB} from './CreateDB'
 export const HomeDB = function (name, version, data) {
   // 三个参数不能为空
   if (name && version && data) {
-    // 判断 data 数组 还是 单个 对象
-    if(data.length){
-        // 解析数组
-        data.map((item, index) => {
+        for(let i in data) {
+            // 解析数组
             let myDB = {
-                name: name,
-                version:  index,
-                db: 'null',
-                ojstore:{
-                    name: item,//存储空间表的名字
-                    keypath:'href'//主键
+                    name: name,
+                    version:  i,
+                    db: 'null',
+                    ojstore:{
+                        name: data[i],//存储空间表的名字
+                        keypath:'href'//主键
+                    }
                 }
-            }
 
             let add =  function () {
-                DB.putData(myDB.db,myDB.ojstore.name, data)
-            }
+                    DB.putData(myDB.db,myDB.ojstore.name, data)
+                }
             // 打开 indexedDB 数据库
             DB.openDB(myDB)
             // 延长2000毫秒 把数据添加到 数据库
             setTimeout(add, 2000)
-        })
+        }
     }
-
-  } else {
-      console.log('name :', 'version :', ' data ：', '不能为空')
-  }
 }
 
