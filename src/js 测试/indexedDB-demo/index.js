b19cec6a8c00199e167e5ca31542896b8f362206
xutao <429499381@qq.com>
2017-09-11 00:11:52 +0800

    加入了 休眠方法

--- src/js 测试/indexedDB-demo/index.js
+++ src/js 测试/indexedDB-demo/index.js
@@ -4,7 +4,7 @@ var bbc = 10
 
 
 function a(callback) {
-   cc = cc + 1
+   // cc = cc + 1
 
 
   setTimeout(function () {
@@ -45,6 +45,26 @@ for (var i = 0; i < 3; i++) {
 
     a(b).then(function (num) {
       console.log('Promise a  setTimeou resolve', num)
+    }).then(function () {
+      console.log('Promise 第二个then')
+      return (function () {
+         return bbc++
+      })()
+    }).then((sleep(3000))()).then(function (num) {
+      console.log('OK', num)
+    }).then(function () {
+      console.log('Ok +++')
     })
 
 }
+
+
+
+function sleep(ms) {
+  return function () {
+    return new Promise(function (resolve, reject) {
+      setTimeout(resolve, ms || 1000)
+    })
+  }
+}
+
