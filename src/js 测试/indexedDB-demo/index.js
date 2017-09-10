3754fc58a1967e0e1fa696c129b8c649df1d18ef
xutao <429499381@qq.com>
2017-09-10 22:51:45 +0800

    回调与setTimeout 在异步函数中，  变量共享与父函数，  如果外部有循环， 所有同步操作都回连续执行 i* N次  a 函数中  cc = cc + 1  实际要执行三次累加  等于13 才回执行下面的异步。  加入把    cc = cc + 1 放在 函数 return 中情况一样的， 详情可以理解 JS 运行机制与顺序。

--- src/js 测试/indexedDB-demo/index.js
+++ src/js 测试/indexedDB-demo/index.js
@@ -1,8 +1,11 @@
 
 var cc = 10
+var bbc = 10
 
 
 function a(callback) {
+   cc = cc + 1
+
 
   setTimeout(function () {
          cc = cc + cc
@@ -10,15 +13,26 @@ function a(callback) {
         console.log('setTimeout CC', cc)
         // cc = 0
       },0)
-
   callback.call(this, cc)
 
+  return new Promise(function (resolve, reject) {
+      // console.log('new Promise a return', cc++)
+
+
+
+    setTimeout(function () {
+      console.log('Promise a  setTimeou return')
+
+      return resolve(cc)
+    },0)
+  })
 
 }
 
 function b(e) {
   setTimeout(function () {
-    console.log('b', e)
+    var bb= e * e
+    console.log('b', bb)
   },0)
 }
 for (var i = 0; i < 3; i++) {
@@ -29,6 +43,8 @@ for (var i = 0; i < 3; i++) {
       },0)
     })(i)
 
-    a(b)
+    a(b).then(function (num) {
+      console.log('Promise a  setTimeou resolve', num)
+    })
 
 }
