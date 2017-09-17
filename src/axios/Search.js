673ebbb75cbf46b8c0afc4f17e51f76468ecf0fc
xutao <429499381@qq.com>
2017-09-17 23:57:14 +0800

    搜索内容  三个字段OK  处理没有正确获取的字段。

--- src/axios/Search.js
+++ src/axios/Search.js
@@ -47,11 +47,9 @@ export  const SearchData = (id,more) => {
 
             })
         }
-        if(more === 'user'){
-
-        }
-        if(more === 'voice'){
-            let more = `${id}/${more}`
+        if(more){
+            let user = `${id}/user`
+            let voice = `${id}/voice`
 
             Search(id).then((res) => {
                 // 列表 标签
@@ -82,26 +80,62 @@ export  const SearchData = (id,more) => {
                         let Title = Title2 ? Title2 : ''
                         Object.assign(SearchData.album[TitleNum],{Title, Title1})
                         TitleNum ++
-                        return OK = 'OK'
+                        // return OK = 'OK'
                     })
                 })
 
 
             })
-            Search(more).then(res => {
-                    const Section_user = /<li\b.+data-url="(.+)">[\d\D]+?<div\b\s+class="pic.+?sound_id="(\d+)"\s+sound_url="(http.+)?"[\d\D]+?src="(http.+.jpg)"[\d\D]+?class="time-cont">(.+)<\/p>[\d\D]+?class="tit">(.+)<\/p>([\d\D]+?class="mgr-5"><i\b.+<\/i>(.+)<\/span>){3}/g
-                    const Section_user1 = /<li\b.+data-url="(.+)">[\d\D]+?<div\b\s+class="pic.+?sound_id="(\d+)"\s+sound_url="(http.+)?"[\d\D]+?src="(http.+.jpg)"[\d\D]+?class="time-cont">(.+)(<\/p>[\d\D]+?class="tit">.+<\/p>)(?:[\d\D]+?class="mgr-5"><i\b.+<\/i>(.+)<\/span>){3}/g
-                    SearchData.voidce = []
-                    res.data.replace(Section_user1, function (match, Href, SoundId, SoundUrl, ImgSrc, TimeCont, Title,  Sms, Mgr) {
-                        SearchData.voidce.push({Href, SoundId, SoundUrl, ImgSrc, TimeCont, Title,  Sms, Mgr})
-                        return OK = 'OK'
-                    })
-                })
+            Search(user).then((res) => {
+            // 列表 标签
+            // const Lireg = /(?!<\/?li>)<li\b\s?class="item\s?[\d\D]+?(<\/li\b>)/g
+            const Lireg = /(?!<\/?li>)<li\b\s?class="item\s?[\d\D]+?href="(.+)">[\d\D]+?(<\/li\b>)/g
+            const ImgSrc = /src\b="(.+)">/g
+            const Playnums = /<span\sclass="mgr-5">(.+)<\/span>/g
+            const Title = /<p\sclass="name\s[\d\D]+?(<span\b\sclass="tag-pay.+">(.+)<\/span>)?\s+(.+)\s+<\/p>/g
+
+
+            // 提取 列表 数据
+            let PNum = 0
+            let ImgNum = 0
+            let TitleNum = 0
+            SearchData.user = []
+            res.data.replace(Lireg, function (match, Href) {
+              // 提取 路由 地址
+              SearchData.user.push({ Href })
+              // 提取 标题 单条数据节目数量
+              match.replace(Playnums, function (data,  PlayNum) {
+                Object.assign(SearchData.user[PNum],{ PlayNum})
+                PNum ++
+              })
+              match.replace(ImgSrc, function (data, ImgSrc) {
+                Object.assign(SearchData.user[ImgNum],{ ImgSrc})
+                ImgNum ++
+              })
+              match.replace(Title, function (data, Title0,Title2, Title1) {
+                let Title = Title2 ? Title2 : ''
+                Object.assign(SearchData.user[TitleNum],{Title, Title1})
+                TitleNum ++
+                // return OK = 'OK'
+              })
+            })
+
+
+          })
+            Search(voice).then(res => {
+            const Section_user = /<li\b.+data-url="(.+)">[\d\D]+?<div\b\s+class="pic.+?sound_id="(\d+)"\s+sound_url="(http.+)?"[\d\D]+?src="(http.+.jpg)"[\d\D]+?class="time-cont">(.+)<\/p>[\d\D]+?class="tit">(.+)<\/p>([\d\D]+?class="mgr-5"><i\b.+<\/i>(.+)<\/span>){3}/g
+            const Section_user1 = /<li\b.+data-url="(.+)">[\d\D]+?<div\b\s+class="pic.+?sound_id="(\d+)"\s+sound_url="(http.+)?"[\d\D]+?src="(http.+.jpg)"[\d\D]+?class="time-cont">(.+)(<\/p>[\d\D]+?class="tit">.+<\/p>)(?:[\d\D]+?class="mgr-5"><i\b.+<\/i>(.+)<\/span>){3}/g
+            SearchData.voice = []
+            res.data.replace(Section_user1, function (match, Href, SoundId, SoundUrl, ImgSrc, TimeCont, Title,  Sms, Mgr) {
+              SearchData.voice.push({Href, SoundId, SoundUrl, ImgSrc, TimeCont, Title,  Sms, Mgr})
+              return OK = 'OK'
+            })
+          })
         }
 
 
 
-      time  = setInterval(Go,50)
+      time  = setInterval(Go,100)
       setTimeout(function () {
         clearInterval(time)
       },5000)
