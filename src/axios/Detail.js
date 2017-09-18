bd8ec092e8c716042d5c90aa90be74f76678aa1b
xutao <429499381@qq.com>
2017-09-18 18:39:24 +0800

    详情页 提取...  部分没有正确匹配存档

--- src/axios/Detail.js
+++ src/axios/Detail.js
@@ -37,25 +37,49 @@ export  const DetailData = (id) => {
 
 
                 // 相关专辑提取
-                const SectionX = /(?!<\/?section>)<section class="mod relativ.album">[\d\D]+?(<\/section\b>)/g
+                const SectionX = /(?!<\/?section>)<section\b\s?class="mod\srelativeAlbum">[\d\D]+?(<\/section\b>)/g
                 const HrefX = /<a\s.+href="([^<>"]+)"/g
                 // 提取 播放量 可播放集数
-                const PlayNumX = /(?:<span.+<\/i>([^<>"]+)<\/span>){1}/g
+                const PlayNumX = /<span.+<\/i>([^<>"]+)<\/span>\s+<span.+<\/i>([^<>"]+)<\/span>/g
                 const TitleX = /<h4\b.+?([^><]+?)</g
 
                 // 播放列表
                 const LiregP = /(?!<\/?li>)<li\b\s?class="item\s?[\d\D]+?(<\/li\b>)/g
-                const MetaP = /<meta[\d\D]+?content="([^';"<>]+)">\s\n/g
+                const MetaP = /<meta[\d\D]+?content="([^';"<>]+)">/g
                 const HrefP = /<a\s.+href="([^<>"]+)"/g
                 const ImsrcP = /<a\s.+sound_url="([^<>"]+)".+sound_id="([^<>"]+)"/g
                 const TitleP = /<h4\b.+?([^><]+?)</g
 
+                 // 相关专辑提取
+                 data.replace(SectionX, function (match) {
+                     let NumH = 0
+                     let NumP = 0
+                     let NumT = 0
+                     GetData.album.MoreAlbum = []
+                     GetData.album.MoreAlbum[NumH] = {}
+                     GetData.album.MoreAlbum[NumP] = {}
+                     GetData.album.MoreAlbum[NumT] = {}
+                     match.replace(HrefX, function (match, Href) {
+                         GetData.album.MoreAlbum[NumH] = {Href}
+                            NumH++
+                     })
+
+                     match.replace(TitleX, function (match, Title) {
+                         Object.assign(GetData.album.MoreAlbum[NumT], {Title})
+                            NumT++
+                     })
+                     match.replace(PlayNumX, function (match, PlayNUm, PlayTime) {
+                         Object.assign(GetData.album.MoreAlbum[NumP], {PlayNUm, PlayTime})
+                             NumP++
+                     })
+                 })
+
                 // 提取 详情内容
-                let NumZ = 0
                 data.replace(SectionD, function (match) {
-                    let Detail = GetData.album
+                    GetData.album.Detail= {}
+                    let Detail = GetData.album.Detail
                     match.replace(TitleD, function (match, Title) {
-                        GetData.album.push = {Title}
+                        Object.assign(Detail,{Title})
                     })
                     match.replace(HrefD, function (match, Href) {
                         Object.assign(Detail, {Href})
@@ -75,46 +99,51 @@ export  const DetailData = (id) => {
                     match.replace(nameD, function (match, name) {
                         Object.assign(Detail, {name})
                     })
-
-                    NumZ++
+                    return OK ='OK'
                 })
 
-                // 相关专辑提取
-                data.replace(SectionX, function (match) {
-                    GetData.album.MoreAlbum = []
-                    let Detail = GetData.album.MoreAlbum
-                    match.replace(HrefX, function (match, Href) {
-                        Detail.push({Href})
-                    })
-                    match.replace(PlayNumX, function (match, PlayNUm, PlayTime) {
-                        Detail.push({PlayNUm, PlayTime})
-                    })
-                    match.replace(TitleX, function (match, Title) {
-                        Detail.push({Title})
-                    })
-                })
+
 
                 // 播放列表
                 data.replace(LiregP, function (match) {
+                    let NumM = 0
+                    let NumM1 = 0
+                    let NumM2 = 0
+                    let NumH = 0
+                    let NumI = 0
+                    let NumT = 0
                     GetData.album.Lists = []
                     let Detail = GetData.album.Lists
-                    match.replace(MetaP, function (match, num1, num2 ,num3, num4) {
-                        Detail.push({num1, num2 ,num3, num4})
+                    match.replace(MetaP, function (match, name) {
+                        GetData.album.Lists[NumH] = {}
+                        Object.assign(Detail[NumH], {name})
+                        NumM++
+                    })
+                    match.replace(MetaP, function (match, time) {
+                        Object.assign( Detail[NumM1], {time})
+                        NumM1++
+                    })
+                    match.replace(MetaP, function (match, img) {
+                        Object.assign( Detail[NumM2], {img})
+                        NumM2++
                     })
                     match.replace(HrefP, function (match, Href) {
-                        Detail.push({Href})
+                        // Detail.push({Href})
+                        Object.assign( Detail[NumH], {Href})
+                        NumH++
                     })
                     match.replace(ImsrcP, function (match, ImgSrc) {
-                        Detail.push({ImgSrc})
+                        Object.assign( Detail[NumH], {ImgSrc})
+                        NumI++
                     })
                     match.replace(TitleP, function (match, Title) {
-                        Detail.push({Title})
+                        Object.assign( Detail[NumT], {Title})
+                        NumT++
                     })
 
                     return OK = 'OK'
                 })
             })
-
       // 5秒后退出 定时
       time  = setInterval(Go,100)
       setTimeout(function () {
