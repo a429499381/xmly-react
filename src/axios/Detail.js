552f27efaacce1374ebf55c444d3602cf6558732
xutao <429499381@qq.com>
2017-09-18 16:14:54 +0800

    详情页 提取...  部分没有正确匹配存档

--- src/axios/Detail.js
+++ src/axios/Detail.js
@@ -2,7 +2,7 @@ import {GetId} from './get.js'
 
 export  const DetailData = (id) => {
     var GetData = {}
-    GetData.album = {}
+    GetData.album = []
     let OK = 'No'
     let time = 'No'
 
@@ -15,10 +15,9 @@ export  const DetailData = (id) => {
                 let data = res.data
                 // 列表 标签
 
-                // 提取 详情内容
+                // 提取 详情 主播介绍
                 const SectionD = /(?!<\/?section>)<section\b\s?class="mod\suserInfo?[\d\D]+?(<\/section\b>)/g
-                const jianjieD = /<div\sclass="intro-breviary">[^><]+<\/div>/g
-                const TitleD = /<h3\sclass="sub-header">[^<>]+<\/h3>/g
+                const TitleD = /<h3\sclass="sub-header">([^<>]+)<\/h3>/g
                 const HrefD = /<a\s.+href="([^<>"]+)"/g
                 const ImgD = /<img\b.+?src="([^><"]+?)"/g
                 const IntroD = /<p\b.+?([^><]+?)<\/p>/g
@@ -26,9 +25,19 @@ export  const DetailData = (id) => {
                 const TxtD = /<div\sclass="intro-breviary\sj-zintro">([^><]+?)</g
                 const TxtFullD = /<div\sclass="intro-full\sj-zintro\suserIntro">([^><]+?)</g
 
+                // 内容简介
+                const SectionJ = /(?!<\/?section>)<section\b\s?class="mod">[\d\D]+?(<\/section\b>)/g
+                const jianjieJ = /<div\sclass="intro-breviary">[^><]+<\/div>/g
+                const TitleJ = /<h3\sclass="sub-header">[^<>]+<\/h3>/g
+                const ImgSrcJ = /src="([^';"]+)"/g
+                // 提取所有 标签内的 内容
+                const All  = /<[\d\D]+?>/g
+
+
+
 
                 // 相关专辑提取
-                const SectionX = /(?!<\/?section>)<section class="mod relativeAlbum">[\d\D]+?(<\/section\b>)/g
+                const SectionX = /(?!<\/?section>)<section class="mod relativ.album">[\d\D]+?(<\/section\b>)/g
                 const HrefX = /<a\s.+href="([^<>"]+)"/g
                 // 提取 播放量 可播放集数
                 const PlayNumX = /(?:<span.+<\/i>([^<>"]+)<\/span>){1}/g
@@ -42,30 +51,32 @@ export  const DetailData = (id) => {
                 const TitleP = /<h4\b.+?([^><]+?)</g
 
                 // 提取 详情内容
+                let NumZ = 0
                 data.replace(SectionD, function (match) {
-                    GetData.album.Detail = []
-                    let Detail = GetData.album.Detail
-                    match.replace(jianjieD, function (match, Txt) {
-                        Detail.push({Txt})
+                    let Detail = GetData.album
+                    match.replace(TitleD, function (match, Title) {
+                        GetData.album.push = {Title}
                     })
-                    match.replace(jianjieD, function (match, Title) {
-                        Detail.album.push({Title})
+                    match.replace(HrefD, function (match, Href) {
+                        Object.assign(Detail, {Href})
                     })
-                    match.replace(jianjieD, function (match, Href) {
-                        Detail.album.push({Href})
+                    match.replace(ImgD, function (match, Img) {
+                        Object.assign(Detail, {Img})
                     })
-                    match.replace(jianjieD, function (match, Img) {
-                        Detail.album.push({Img})
+                    match.replace(IntroD, function (match, Intro) {
+                        Object.assign(Detail, {Intro})
                     })
-                    match.replace(jianjieD, function (match, IntroD) {
-                        Detail.album.push({IntroD})
+                    match.replace(TxtD, function (match, Txt) {
+                        Object.assign(Detail, {Txt})
                     })
-                    match.replace(jianjieD, function (match, Txt1) {
-                        Detail.album.push({Txt1})
+                    match.replace(TxtFullD, function (match, TxtFull) {
+                        Object.assign(Detail, {TxtFull})
                     })
-                    match.replace(jianjieD, function (match, TxtFullD) {
-                        Detail.album.push({TxtFullD})
+                    match.replace(nameD, function (match, name) {
+                        Object.assign(Detail, {name})
                     })
+
+                    NumZ++
                 })
 
                 // 相关专辑提取
