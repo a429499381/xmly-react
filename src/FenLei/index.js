17c8ee199b998ef3b5028fd81ca55f8bfa9194c8
xutao <429499381@qq.com>
2017-09-24 01:05:01 +0800

    引入 LeanCound 后端服务  注册  登陆 登出  查询是否登陆 OK

--- src/FenLei/index.js
+++ src/FenLei/index.js
@@ -4,7 +4,7 @@ import Header from '../PageCom/Header'
 import FooterNavgtion from '../PageCom/FooterNavgtion'
 import NoMore from '../PageCom/NoMore'
 
-import {a} from 'leancloud-storage'
+import {AV} from 'leancloud-storage'
 
 class FenLei extends Component {
   render() {
@@ -20,6 +20,17 @@ class FenLei extends Component {
           <NoMore/>
     </div>
     )
+  }
+  componentDidMount() {
+    var APP_ID = '3X9jTyLPjORGtQIdFneYartA-gzGzoHsz';
+    var APP_KEY = '9XKG8fPj4NlLgIWf71AWrQeD';
+
+    AV.init({
+      appId: APP_ID,
+      appKey: APP_KEY
+    });
+
+    console.log(AV)
   }
 }
 
