57811f22714c5dcb7528a5e1f90a49574163eb07
xutao <429499381@qq.com>
2017-09-24 22:28:58 +0800

    播放页面  播放 暂停 制定播放当前时间 加速 减速度

--- src/FenLei/index.js
+++ src/FenLei/index.js
@@ -17,12 +17,11 @@ class FenLei extends Component {
 
               <div className="mt86"></div>
               <h4 classID="name">李玉刚 - 刚好遇见你1</h4>
-              <audio classID="audio" src="http://audio.xmcdn.com/group32/M08/05/87/wKgJnFnFHgmA3tnSAHEVQqq3e00956.m4a"
-                     controls autoplay="true"></audio>
-              <button classID="btn-play">播放</button>
-              <button classID="btn-stop">暂停</button>
-              <button classID="btn-pre">上一首</button>
-              <button classID="btn-next">下一首</button>
+              <audio classID="audio" ref='audio' controls src="http://audio.xmcdn.com/group32/M08/05/87/wKgJnFnFHgmA3tnSAHEVQqq3e00956.m4a"></audio>
+              <button classID="btn-play" onClick={this.play.bind(this)}>播放</button>
+              <button classID="btn-stop" onClick={this.Seek.bind(this)}>暂停</button>
+              <button classID="btn-pre" onClick={this.Up.bind(this)}>上一首</button>
+              <button classID="btn-next" onClick={this.Dp.bind(this)}>下一首</button>
           </div>
 
           分类页面
@@ -33,17 +32,40 @@ class FenLei extends Component {
     </div>
     )
   }
-  // componentDidMount() {
-  //   var APP_ID = '3X9jTyLPjORGtQIdFneYartA-gzGzoHsz';
-  //   var APP_KEY = '9XKG8fPj4NlLgIWf71AWrQeD';
-  //
-  //   AV.init({
-  //     appId: APP_ID,
-  //     appKey: APP_KEY
-  //   });
-  //
-  //   console.log(AV)
-  // }
+
+  componentDidMount() {
+    window.N = false
+    var src="http://audio.xmcdn.com/group32/M08/05/87/wKgJnFnFHgmA3tnSAHEVQqq3e00956.m4a";
+    window.audio = new Audio(src)
+  }
+  play() {
+    let audio = this.refs.audio
+    //  设置当前播放位置 不设置就是获取当前播放位置
+    let star = audio.currentTime = 600
+    console.log(audio.currentTime)
+    audio.paused ? audio.play() : audio.pause()
+  }
+
+  Seek() {
+    let audio = window.audio
+    audio.paused ? audio.play() : audio.pause()
+    console.log(audio.duration)
+  }
+  Up() {
+    let audio = window.audio
+    audio.playbackRate = audio.playbackRate + 0.2
+    if (audio.playbackRate >= 2) {
+      audio.playbackRate = 1
+    }
+    console.log(audio.playbackRate)
+  }
+
+  Dp() {
+    let audio = window.audio
+    let star = audio.startTime
+    star = 600
+    console.log(audio.startTime)
+  }
 }
 
 export default FenLei;
