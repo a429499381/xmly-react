f9f8156f01b8f581ef0ee77413f44deb2dedc318
xutao <429499381@qq.com>
2017-09-24 12:02:58 +0800

    ...

--- src/FenLei/index.js
+++ src/FenLei/index.js
@@ -4,7 +4,7 @@ import Header from '../PageCom/Header'
 import FooterNavgtion from '../PageCom/FooterNavgtion'
 import NoMore from '../PageCom/NoMore'
 
-import {AV} from 'leancloud-storage'
+// import {AV} from 'leancloud-storage'
 
 class FenLei extends Component {
   render() {
@@ -13,6 +13,18 @@ class FenLei extends Component {
         {/* top */}
           <Header/>
         <div className="mt86"></div>
+          <div>
+
+              <div className="mt86"></div>
+              <h4 classID="name">李玉刚 - 刚好遇见你</h4>
+              <audio classID="audio" src="http://audio.xmcdn.com/group32/M08/05/87/wKgJnFnFHgmA3tnSAHEVQqq3e00956.m4a"
+                     controls autoplay="true"></audio>
+              <button classID="btn-play">播放</button>
+              <button classID="btn-stop">暂停</button>
+              <button classID="btn-pre">上一首</button>
+              <button classID="btn-next">下一首</button>
+          </div>
+
           分类页面
         {/* 底部导航 */}
           <FooterNavgtion/>
