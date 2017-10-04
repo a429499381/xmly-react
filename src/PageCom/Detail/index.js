44601a7e901476996511ef5021f909624e6781ad
xutao <429499381@qq.com>
2017-10-04 23:32:24 +0800

    详情页  之前保存出错。

--- src/PageCom/Detail/index.js
+++ src/PageCom/Detail/index.js
@@ -4,9 +4,9 @@
 import React, {Component} from 'react'
 
 import Back from '../../PageCom/Back'
-import 'index.css'
+import './index.css'
 
-class Detail extends Component {
+export default class Detail extends Component {
   constructor(props) {
     super(props)
   }
@@ -15,27 +15,45 @@ class Detail extends Component {
     return (
       <div>
         <Back/>
-        <h4 className="title">中国男子在</h4>
-        <div className="paly">
-          <img src="" alt="" className="play"/>
-          <span className="zhubo">主播：东方网</span>
-          <span className="playScroll">
-            <span className="start">00:00</span>
-            <span className="startIng"></span>
-            <span className="end">00:00</span>
-          </span>
-        </div>
-        <p className="samllTitle">一日之计在于晨， 新闻早餐不能少.</p>
-        <p className="samllTitle">微信公众号： xwzc021</p>
+        <div className="DTitle">
+          <h2 className="title">中国男子在</h2>
+          <div className="paly">
+            <img src="" alt="" className="playImg"/>
+            <p className="zhubo">主播：东方网</p>
+            <div className="playScroll">
+              <span className="start">00:00</span>
+              <span className="startIng">
+                <a href="#" className="red"></a>
+              </span>
+              <span className="end">16:00</span>
+            </div>
+          </div>
+          <div className="fix pt20"></div>
+          <p className="samllTitle">一日之计在于晨， 新闻早餐不能少.</p>
+          <p className="samllTitle">微信公众号： xwzc021</p>
 
-        <div className="blums">
-          <div className="lists">
-            <p className="item">
-              625辆出租车顶灯滚动播：老婆我错了。 结果...
-            </p>
-            <a href="#" className="itemIcon"></a>
+          <div className="blums">
+            <div className="lists">
+              <p className="item">
+                625辆出租车顶灯滚动播：老婆我错了。 结果...
+              </p>
+            </div>
+            <a href="#" className="itemIcon">
+              <i className="paly"></i>
+            </a>
+          </div>
+          <div className="blums">
+            <div className="lists">
+              <p className="item">
+                625辆出租车顶灯滚动播：老婆我错了。 结果...
+              </p>
+            </div>
+            <a href="#" className="itemIcon">
+              <i className="paly"></i>
+            </a>
           </div>
         </div>
+
       </div>
     )
   }
