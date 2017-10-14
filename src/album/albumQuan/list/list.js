bfe216b5005d13d2035f71baca4fc182f08baefa
xutao <429499381@qq.com>
2017-10-14 12:58:00 +0800

    album-quan/* 页面样式 OK 开始提取数据 添加路由

--- src/album/albumQuan/list/list.js
+++ src/album/albumQuan/list/list.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import LoadIcon from  '../../../PageCom/loadIcon'
+import {Link} from 'react-router'
 import '../list/list.scss'
 
 class AlbumQuan extends Component {
@@ -10,6 +11,7 @@ class AlbumQuan extends Component {
             rank: '',
             recent:'',
             classic: '',
+            url: ''
         }
     }
     render() {
@@ -17,12 +19,44 @@ class AlbumQuan extends Component {
         return (
             <div className="albumQuanList fix">
                 <div className="albumQuannav">
-                    <span className="item active">最火</span>
-                    <span className="item">最近更新</span>
-                    <span className="item">经典</span>
+                        <span className="item active">最火</span>
+                        <span className="item active">最近更新</span>
+                        <span className="item active">经典</span>
+
                 </div>
                 <div className="Quanlist">
-
+                    <div className="list p5 rel mt5 mb5">
+                            <img src="" alt="" className="img"/>
+                        <div className="container ">
+                            <h4>《白话全本红楼梦》韦岽:著 精致女王：播</h4>
+                            <p className="palyNum">10919232</p>
+                            <i className="down"></i>
+                        </div>
+                    </div>
+                    <div className="list p5 rel mt5 mb5">
+                        <img src="" alt="" className="img"/>
+                        <div className="container ">
+                            <h4>《白话全本红楼梦》韦岽:著 精致女王：播</h4>
+                            <p className="palyNum">10919232</p>
+                            <i className="down"></i>
+                        </div>
+                    </div>
+                    <div className="list p5 rel mt5 mb5">
+                        <img src="" alt="" className="img"/>
+                        <div className="container ">
+                            <h4>《白话全本红楼梦》韦岽:著 精致女王：播</h4>
+                            <p className="palyNum">10919232</p>
+                            <i className="down"></i>
+                        </div>
+                    </div>
+                    <div className="list p5 rel mt5 mb5">
+                        <img src="" alt="" className="img"/>
+                        <div className="container ">
+                            <h4>《白话全本红楼梦》韦岽:著 精致女王：播</h4>
+                            <p className="palyNum">10919232</p>
+                            <i className="down"></i>
+                        </div>
+                    </div>
                 </div>
             </div>
         )
